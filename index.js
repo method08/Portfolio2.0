b5ccd289e6600a52ad23881d049ace6d49dc3fc4
method08 <coachdanielf@gmail.com>
2022-09-07 07:19:19 -0400

    added pressed-appearance functionality & styling to buttons

--- index.js
+++ index.js
@@ -9,18 +9,57 @@ const showContact = () => {
     contactBox.style.display = 'block';
     portfolioBox.style.display = 'none';
     aboutBox.style.display = 'none';
-}
-
-const showAbout = () => {
-    contactBox.style.display = 'none';
-    portfolioBox.style.display = 'none';
-    aboutBox.style.display = 'block';
+    contactButton.style.backgroundColor = 'black';
+    contactButton.style.color = 'greenyellow';
+    contactButton.style.border = '1px solid greenyellow';
+    portfolioButton.style.backgroundColor = 'greenyellow';
+    portfolioButton.style.color = 'black';
+    portfolioButton.style.border = '2px solid black';
+    portfolioButton.style.borderBottom = '5px solid black';
+    portfolioButton.style.borderTop = '1px solid black';    
+    aboutButton.style.backgroundColor = 'greenyellow';
+    aboutButton.style.color = 'black';
+    aboutButton.style.border = '2px solid black';
+    aboutButton.style.borderBottom = '5px solid black';
+    aboutButton.style.borderTop = '1px solid black';  
 }
 
 const showPortfolio = () => {
     contactBox.style.display = 'none';
+    portfolioBox.style.display = 'none';
+    aboutBox.style.display = 'block';
+    portfolioButton.style.backgroundColor = 'black';
+    portfolioButton.style.color = 'greenyellow';
+    portfolioButton.style.border = '1px solid greenyellow';
+    contactButton.style.backgroundColor = 'greenyellow';
+    contactButton.style.color = 'black';
+    contactButton.style.border = '2px solid black';
+    contactButton.style.borderBottom = '5px solid black';
+    contactButton.style.borderTop = '1px solid black';    
+    aboutButton.style.backgroundColor = 'greenyellow';
+    aboutButton.style.color = 'black';
+    aboutButton.style.border = '2px solid black';
+    aboutButton.style.borderBottom = '5px solid black';
+    aboutButton.style.borderTop = '1px solid black';  
+}
+
+const showAbout = () => {
+    contactBox.style.display = 'none';
     portfolioBox.style.display = 'block';
     aboutBox.style.display = 'none';
+    aboutButton.style.backgroundColor = 'black';
+    aboutButton.style.color = 'greenyellow';
+    aboutButton.style.border = '1px solid greenyellow';
+    portfolioButton.style.backgroundColor = 'greenyellow';
+    portfolioButton.style.color = 'black';
+    portfolioButton.style.border = '2px solid black';
+    portfolioButton.style.borderBottom = '5px solid black';
+    portfolioButton.style.borderTop = '1px solid black';    
+    contactButton.style.backgroundColor = 'greenyellow';
+    contactButton.style.color = 'black';
+    contactButton.style.border = '2px solid black';
+    contactButton.style.borderBottom = '5px solid black';
+    contactButton.style.borderTop = '1px solid black';  
 }
 
 contactButton.onclick = showContact;
