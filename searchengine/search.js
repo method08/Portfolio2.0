66f5350dd231fa60f8b0dbb9723490aa04258955
method08 <coachdanielf@gmail.com>
2022-09-10 10:52:21 -0400

    almost finished search engine project - stuck debugging color filters, they won't function

--- searchengine/search.js
+++ searchengine/search.js
@@ -1,6 +1,98 @@
-let redButton = document.getElementsByClassName('red');
-let blueButton = document.getElementsByClassName('blue');
-let orangeButton = document.getElementsByClassName('orange');
+const filterChecks = [document.querySelector("#redFilter"), 
+document.querySelector("#blueFilter"), 
+document.querySelector("#orangeFilter"),
+document.querySelector('#squareFilter'),
+document.querySelector('#circleFilter'),
+document.querySelector('#rectangleFilter')];
 
-// dumbass! the querySelector will select the FIRST element it finds! //
+const filterButtons = [document.querySelector('.redButton'),
+document.querySelector('.blueButton'),
+document.querySelector('.orangeButton'),
+document.querySelector('.squareButton'),
+document.querySelector('.circleButton'),
+document.querySelector('.rectangleButton')];
 
+const redShapes = document.querySelectorAll('.red');
+const blueShapes = document.querySelectorAll('.blue');
+const orangeShapes = document.querySelectorAll('.orange');
+console.log(orangeShapes, blueShapes, redShapes);
+const squareShapes = document.querySelectorAll('.square');
+const circleShapes = document.querySelectorAll('.circle');
+const rectangleShapes = document.querySelectorAll('.rectangle');
+
+// console.log(filterButtons);
+// console.log(filterChecks);
+// console.log(document.querySelector('#redFilter'));
+
+ function buttonPush() {
+    for (let i=0; i < filterChecks.length; i++) {
+        console.log(filterChecks[i])
+        console.log(filterChecks[i].checked)
+        if (filterChecks[i].checked === true) {
+            filterButtons[i].style.backgroundColor = 'black';
+            filterButtons[i].style.color = 'lightgreen';
+        }
+        else {filterButtons[i].style.backgroundColor = 'lightgreen'; 
+            filterButtons[i].style.color = 'black'}
+    }
+ } 
+
+ function setFilter() {
+        for (let k=0; k < filterChecks.length; k++) {
+            if (k === 0 && filterChecks[k].checked === true) {
+                for (let j=0; j < redShapes.length; j++)
+                redShapes[j].style.display = 'none';
+            } 
+            if (k === 0 && filterChecks[k].checked === false) {
+                for (let j=0; j < redShapes.length; j++)
+                redShapes[j].style.display = 'block';
+            } 
+            if (k === 1 && filterChecks[k].checked === true) {
+                for (let j=0; j < blueShapes.length; j++)
+                blueShapes[j].style.display = 'none';
+        } 
+            if (k === 1 && filterChecks[k].checked === false) {
+                for (let j=0; j < blueShapes.length; j++)
+                blueShapes[j].style.display = 'block';
+        } 
+            if (k === 2 && filterChecks[k].checked === true) {
+                for (let j=0; j < orangeShapes.length; j++)
+                orangeShapes[j].style.display = 'none';
+        } 
+            if (k === 2 && filterChecks[k].checked === false) {
+                for (let j=0; j < orangeShapes.length; j++)
+                orangeShapes[j].style.display = 'block';
+        } 
+            if (k === 3 && filterChecks[k].checked === true) {
+                for (let j=0; j < squareShapes.length; j++)
+                squareShapes[j].style.display = 'none';
+        } 
+            if (k === 3 && filterChecks[k].checked === false) {
+                for (let j=0; j < squareShapes.length; j++)
+                squareShapes[j].style.display = 'block';
+        } 
+            if (k === 4 && filterChecks[k].checked === true) {
+                for (let j=0; j < circleShapes.length; j++)
+                circleShapes[j].style.display = 'none';
+        } 
+            if (k === 4 && filterChecks[k].checked === false) {
+                for (let j=0; j < circleShapes.length; j++)
+                circleShapes[j].style.display = 'block';
+        } 
+            if (k === 5 && filterChecks[k].checked === true) {
+                for (let j=0; j < rectangleShapes.length; j++)
+                rectangleShapes[j].style.display = 'none';
+        } 
+            if (k === 5 && filterChecks[k].checked === false) {
+                for (let j=0; j < rectangleShapes.length; j++)
+                rectangleShapes[j].style.display = 'block';
+        } 
+    }
+ }
+
+filterChecks[0].addEventListener('change', function() {buttonPush(), setFilterRed()}); 
+filterChecks[1].addEventListener('change', function() {buttonPush(), setFilter()}); 
+filterChecks[2].addEventListener('change', function() {buttonPush(), setFilter()}); 
+filterChecks[3].addEventListener('change', function() {buttonPush(), setFilter()}); 
+filterChecks[4].addEventListener('change', function() {buttonPush(), setFilter()}); 
+filterChecks[5].addEventListener('change', function() {buttonPush(), setFilter()}); 
